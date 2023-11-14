29fc6a9ffae0f527d375203b42f46988b8fa2981
Kristawan Jaroenrad <kristawanja@gmail.com>
2023-11-14 21:29:40 +0700

    add gen_comb_list_recursive function

--- data_processing.py
+++ data_processing.py
@@ -179,3 +179,23 @@ survival_m = sum([1 if i["survived"] == 'yes' else 0 for i in male]) / len(male)
 survival_f = sum([1 if i["survived"] == 'yes' else 0 for i in female]) / len(female)
 print(f"male: {survival_m*100:.2f}% vs. female: {survival_f*100:.2f}%")
 print()
+
+
+
+# --task 2--
+import copy
+
+def gen_comb_list_recursive(list_set):
+    if len(list_set) == 1:
+        start_list = []
+        for item in list_set[0]:
+            start_list.append([item])
+        return start_list
+    comb_list_temp = gen_comb_list_recursive(list_set[0:-1])
+    start_list = []
+    for list_item in comb_list_temp:
+        for val in list_set[-1]:
+            temp_item = copy.deepcopy(list_item)
+            temp_item.append(val)
+            start_list.append(temp_item)
+    return start_list
